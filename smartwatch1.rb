51603b0f82fdb772dafb77233167951660d3b529
Jorge <jorgesifontes.cl@gmail.com>
2020-10-28 21:49:31 -0300

    Final smartwatch1.rb

---
+++ smartwatch1.rb
@@ -0,0 +1,18 @@
+def clear_steps (pasos_diarios)
+    pasos_limpios = []
+    
+    pasos_diarios.each do |elemento|
+        if (elemento.count("0-9") == elemento.length)
+            pasos_limpios.push(elemento)
+        end
+    end
+
+    pasos_limpios.map! { |ele| ele.to_i } 
+
+    pasos_limpios.select { |paso| !((paso < 200) || (paso > 100000)) }
+end
+
+pasos = ['100', '21', '231as', '2031', '1052000', '213b', 'b123']
+
+print "Data original = #{pasos}\n"
+print "Data limpiada = #{clear_steps(pasos)}\n"
